acf9c621824ba06a35b1bb6cfa0ef0a1131ccea2
Kym Phillpotts <kphillpotts@gmail.com>
2019-06-27 21:55:19 +1000

    Basic Project Structure - PreStream

---
+++ src/RottenUI/RottenUI/Data/MockRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace RottenUI.Data
+{
+    public static class MockRepository
+    {
+        public static IList<Movie> Movies { get; set; }
+        static MockRepository()
+        {
+            Movies = new ObservableCollection<Movie>
+            {
+                new Movie()
+                {
+                    Title = "Zootopia",
+                    ReleaseDate = "2016",
+                    AudienceScore = 96,
+                    TomatometerScore = 96,
+                    PosterUrl = "https://image.tmdb.org/t/p/w185/bHXf0mrnnRthdWI9MuAMlPftnZK.jpg",
+                    BackdropUrl = "https://image.tmdb.org/t/p/w780/qCQSWM3fhPjSAZEgCQu68LQXna0.jpg",
+                    Info = "Determined to prove herself, Officer Judy Hopps, the first bunny on Zootopia's police force, jumps at the chance to crack her first case - even if it means partnering with scam-artist fox Nick Wilde to solve the mystery.",
+                    Cast = new List<CastMember>()
+                {
+                    new CastMember() { Name="Ginnifer Goodwin", Role="Judy Hopps (voice)", ImageUrl="https://image.tmdb.org/t/p/w185/zioPX1VMA26ucdpvF9STz7m3l4j.jpg"},
+                    new CastMember() { Name="Jason Bateman", Role="Nick Wilde (voice)", ImageUrl="https://image.tmdb.org/t/p/w185/ttzLpjvcLkvXyyTBpjmZw11tjlr.jpg"},
+                    new CastMember() { Name="Shakira", Role="Gazelle (voice)", ImageUrl="https://image.tmdb.org/t/p/w185/lvpKTDdWUv61sty299uC8DQzVTX.jpg"},
+                    new CastMember() { Name="Idris Elba", Role="Chief Bogo (voice)", ImageUrl="https://image.tmdb.org/t/p/w185/be1bVF7qGX91a6c5WeRPs5pKXln.jpg"},
+                }
+                }
+            };
+        }
+    }
+
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string ReleaseDate { get; set; }
+        public int AudienceScore { get; set; }
+        public int TomatometerScore { get; set; }
+        public string Info { get; set; }
+        public List<CastMember> Cast { get; set; }
+        public string PosterUrl { get; set; }
+        public string BackdropUrl { get; set; }
+    }
+
+    public class CastMember
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ src/RottenUI/RottenUI/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RottenUI
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            // super-lame binding to get data for the screen
+            this.BindingContext = Data.MockRepository.Movies[0];
+            InitializeComponent();
+        }
+    }
+}
