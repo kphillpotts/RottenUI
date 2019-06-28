4b752297328bba641d16ae1e3c9eafb8246023b9
Kym Phillpotts <kphillpotts@gmail.com>
2019-06-28 13:40:15 +1000

    Code after stream 1 -

--- src/RottenUI/RottenUI/MainPage.xaml.cs
+++ src/RottenUI/RottenUI/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -18,6 +20,39 @@ namespace RottenUI
             // super-lame binding to get data for the screen
             this.BindingContext = Data.MockRepository.Movies[0];
             InitializeComponent();
+        }
+
+        private void SKCanvasView_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            using (SKPaint paint = new SKPaint())
+            {
+                // define the color for the shadow
+                SKColor shadowColor = Color.FromHex("#5ACB6E").ToSKColor();
+
+                paint.IsDither = true;
+                paint.IsAntialias = true;
+                paint.Color = shadowColor;
+
+                // create filter for drop shadow
+                paint.ImageFilter = SKImageFilter.CreateDropShadow(
+                    dx: 0, dy: 0, 
+                    sigmaX: 40, sigmaY: 40, 
+                    color: shadowColor, 
+                    shadowMode: SKDropShadowImageFilterShadowMode.DrawShadowOnly);
+
+                // define where I want to draw the object
+                var margin = info.Width / 10;
+                var shadowBounds = new SKRect(margin, -40, info.Width - margin, 10);
+                canvas.DrawRoundRect(shadowBounds, 10, 10, paint);
+
+            }
+
         }
     }
 }
---
+++ src/RottenUI/RottenUI/Views/DetailsSection.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RottenUI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailsSection : ContentView
+    {
+        int selectionIndex = 0;
+        List<Label> tabHeaders = new List<Label>();
+        List<VisualElement> tabContents = new List<VisualElement>();
+
+        public DetailsSection()
+        {
+            InitializeComponent();
+
+            tabHeaders.Add(InfoTab);
+            tabHeaders.Add(CastTab);
+            tabHeaders.Add(NewsTab);
+            tabHeaders.Add(CriticsTab);
+            tabHeaders.Add(MediaTab);
+
+            tabContents.Add(InfoContent);
+            tabContents.Add(CastContent);
+            tabContents.Add(NewsContent);
+            tabContents.Add(CriticsContent);
+            tabContents.Add(MediaContent);
+
+
+        }
+
+
+
+        private async Task ShowSelection(int newTab)
+        {
+            if (newTab == selectionIndex) return;
+
+            // navigate the selection pill
+            var selectdTabLabel = tabHeaders[newTab];
+            _ = SelectionUnderline.TranslateTo(selectdTabLabel.Bounds.X, 0, 150, easing:Easing.SinInOut);
+
+            // update the style of the header to show it's selcted
+            var unselectedStyle = (Style)Application.Current.Resources["TabLabel"];
+            var selectedStyle = (Style)Application.Current.Resources["SelectedTabLabel"];
+            tabHeaders[selectionIndex].Style = unselectedStyle;
+            selectdTabLabel.Style = selectedStyle;
+
+            /// reveal the contents
+            await tabContents[selectionIndex].FadeTo(0);
+            tabContents[selectionIndex].IsVisible = false;
+            tabContents[newTab].IsVisible = true;
+            _ = tabContents[newTab].FadeTo(1); //ybadragon thanks!
+
+            selectionIndex = newTab;
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var tabIndex = tabHeaders.IndexOf((Label)sender);
+            await ShowSelection(tabIndex);
+        }
+    }
+}
